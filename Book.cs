4997438e7112324cb8de1cd89b0f4ed31c9cf941
lenhat41555 <70925729+lenhat41555@users.noreply.github.com>
2020-09-28 11:56:43 +0700

    Initial commit

---
+++ Book.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    internal class Book
+    {
+
+    public string ISBN { get; set; }
+    public string Name { get; set; }
+    public string Author { get; set; }
+}
+    }
